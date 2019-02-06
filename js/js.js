b80d5741e744a1195e6b2bcfdb14240f98cd1a5a
ourangelofsorrows <angelezrameyer@gmail.com>
2019-02-06 16:27:04 +0100

    im tired. I've got tired bitch disease

--- js/js.js
+++ js/js.js
@@ -4,8 +4,8 @@ const main = document.querySelector("main");
 for (let i = 0; i<10; i++){
 
     let clone = template.cloneNode(true);
-    clone.querySelector("h1").textContent="Doggo";
-    clone.querySelector("h2").textContent="good woofer";
+    clone.querySelector("h2").textContent="Doggo";
+    clone.querySelector("h3").textContent="good woofer";
     clone.querySelector("img").src="images/download.jpg";
 
     main.appendChild(clone);
