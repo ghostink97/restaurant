40ca7749b554170846a2bb374c5c7521ee714055
ourangelofsorrows <angelezrameyer@gmail.com>
2019-02-08 10:03:28 +0100

    morework

--- js/js.js
+++ js/js.js
@@ -1,15 +1,47 @@
 const template = document.querySelector("template").content;
-const main = document.querySelector("main");
 
-for (let i = 0; i<10; i++){
 
+fetch("http://kea-alt-del.dk/t5/api/productlist").then(e=>e.json()).then(data=>data.forEach(showData))
+
+function showData(oneObject){
+    console.log(oneObject);
     let clone = template.cloneNode(true);
-    clone.querySelector("h2").textContent="Doggo";
-    clone.querySelector("h3").textContent="good woofer";
-    clone.querySelector("img").src="images/download.jpg";
+    clone.querySelector("#name").textContent=oneObject.name;
+    clone.querySelector("#price").textContent=oneObject.price;
+    clone.querySelector("#dishpic").src="imgs/medium/" + oneObject.image + "-md.jpg";
 
+    let main;
+    if(oneObject.category=="starter"){
+        main = document.querySelector("#starter");
+
+    }
+    else if(oneObject.category=="main"){
+        main = document.querySelector("#mainc");
+
+    }
+    else if(oneObject.category=="dessert"){
+        main = document.querySelector("#dessert");
+
+    }
+    else if(oneObject.category=="drinks"){
+        main = document.querySelector("#drinks");
+
+    }
+    else if(oneObject.category=="sideorders"){
+        main = document.querySelector("#sideor");
+
+    }
     main.appendChild(clone);
 }
+
+//**for (let i = 0; i<10; i++){
+//**    let clone = template.cloneNode(true);
+//**    clone.querySelector("h2").textContent="Doggo";
+//**    clone.querySelector("h3").textContent="good woofer";
+//    clone.querySelector("img").src="images/download.jpg";
+
+//    main.appendChild(clone);
+//}
 
 
 
