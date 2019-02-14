f207960a8b5dd27e694d5e77a5c268025489a088
ourangelofsorrows <angelezrameyer@gmail.com>
2019-02-14 10:35:25 +0100

    MORE JS

--- js/js.js
+++ js/js.js
@@ -1,9 +1,10 @@
+
 const template = document.querySelector("template").content;
-let cat1=document.querySelector("#cat1");
-let cat2=document.querySelector("#cat2");
-let cat3=document.querySelector("#cat3");
-let cat4=document.querySelector("#cat4");
-let cat5=document.querySelector("#cat5");
+const cat1=document.querySelector("#cat1");
+const cat2=document.querySelector("#cat2");
+const cat3=document.querySelector("#cat3");
+const cat4=document.querySelector("#cat4");
+const cat5=document.querySelector("#cat5");
 let starter=document.querySelector("#starter");
 let sideor=document.querySelector("#sideor");
 let dessert=document.querySelector("#dessert");
@@ -13,21 +14,45 @@ const moretext=document.querySelector("#moretext");
 const hidevalues=document.querySelector("#hidevalues");
 const modal=document.querySelector(".modal");
 const lessinf=document.querySelector("#lessinf");
+const vegetarian=document.querySelector(".vegetarian");
+
+//API shortcuts
+const productlistLink = "http://kea-alt-del.dk/t5/api/productlist";
+const catLink = "http://kea-alt-del.dk/t5/api/categories";
+const productLink = "http://kea-alt-del.dk/t5/api/product?id=";
+const imgLink = "https://kea-alt-del.dk/t5/site/imgs/";
 
 
 fetch("http://kea-alt-del.dk/t5/api/productlist").then(e=>e.json()).then(data=>data.forEach(showData))
 
 function showData(oneObject){
-    console.log(oneObject);
     let clone = template.cloneNode(true);
     clone.querySelector("#name").textContent=oneObject.name;
     clone.querySelector("#price").textContent=oneObject.price;
     clone.querySelector("#dishpic").src="imgs/medium/" + oneObject.image + "-md.jpg";
 
     const button=document.querySelector("#moreinf");
-clone.querySelector("button").addEventListener("click", ()=>{
-    fetch("http://kea-alt-del.dk/t5/api/productlist").then(e=>e.json()).then(data=>detailsShow(data));
-});
+    clone.querySelector("button").addEventListener("click", ()=>{
+    fetch(productLink+oneObject.id).then(e=>e.json()).then(data=>detailsShow(data));
+        });
+
+    function detailsShow(product){
+        console.log(product);
+        modal.querySelector("h1").textContent=product.name;
+        modal.querySelector("#longdesc").textContent=product.longdescription;
+        modal.querySelector("#discount").textContent=product.price-(product.price*product.discount/100);
+        modal.querySelector("img").src="imgs/medium/" + product.image + "-md.jpg";
+        modal.classList.remove("inactive");
+    }
+
+    function veggieboi(product){
+        if (product.vegetarian==false){
+            vegetarian.classList.add("inactive");
+        }
+    }
+    veggieboi(oneObject);            
+    
+
     let main;
     if(oneObject.category=="starter"){
         main = document.querySelector("#starter");
@@ -51,8 +76,6 @@ clone.querySelector("button").addEventListener("click", ()=>{
     }
     main.appendChild(clone);
 }
-
-
 
 cat1.addEventListener("click", openItem);
 
@@ -84,8 +107,6 @@ function openMenu(){
     secs.classList.toggle("inactive");
 }
 
-
-
 readvalues.addEventListener("click", onpenItemValue);
 hidevalues.addEventListener("click", onpenItemValue);
 
@@ -96,15 +117,6 @@ function onpenItemValue(){
 }
 
 
-function detailsShow(product){
-    console.log(product);
-    modal.querySelector("h1").textContent=product.name;
-    modal.querySelector("#longdesc").textContent=product.longdescription;
-    modal.querySelector("#discount").textContent=product.price-(product.price*product.discount/100);
-    modal.querySelector("img").src="imgs/medium/" + product.image + "-md.jpg";
-    modal.classList.remove("inactive");
-
-}
 
 lessinf.addEventListener("click", ()=>modal.classList.add("inactive"));
 
